dc1e9fee231af2b3b94cddb1c5392767aad3bb56
ola mohamed <olamohamedola276@gmail.com>
2023-08-10 06:09:10 -0700

    commit message

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point
+ * Description: Outputs a statement
+ * Return: 0 if successful
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
