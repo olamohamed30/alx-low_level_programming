752d4ed38653121628022829b0f58bd64325e879
ola mohamed <olamohamedola276@gmail.com>
2023-08-10 06:18:10 -0700

    commit message

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main  Start the program
+* Description: Output a sintence
+* Return: 0 if right
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
