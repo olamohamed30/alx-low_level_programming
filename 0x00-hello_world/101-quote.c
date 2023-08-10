4e39f99e6405a2d469bca6cdd1bdd8bd8239ce92
ola mohamed <olamohamedola276@gmail.com>
2023-08-10 10:39:33 -0700

    commit message

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <unistd.h>
 #include <string.h>
-// ssize_t write(int fd, const void *buf, size_t count);
-
+/**
+* main - Start the program
+* Description: Output a sintence
+* Return: 1 if right
+*/
 int main(void)
 {
-    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, message, strlen(message));
-
-    return (1);
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, message, strlen(message));
+return (1);
 }
