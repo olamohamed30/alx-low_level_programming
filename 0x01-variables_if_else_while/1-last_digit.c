beaff147fcd5e0c50bab16d92e046307414a1b26
ola mohamed <olamohamedola276@gmail.com>
2023-08-11 05:49:36 -0700

    commit message

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,10 +14,10 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 last = n % 10;
 if (last > 5)
-printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
+printf("Last digit of %i is %i and is greater than 5\n", n, last);
 else if (last == 0)
 printf("Last digit of %i is 0 and is 0\n", n);
 else
-printf("last digit of %i is %i and is less than 6 and not 0\n", n, last);
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
 return (0);
 }
