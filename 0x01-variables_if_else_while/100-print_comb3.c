7b1a945c19ecd730c64da70a6b3b004f8b884535
ola mohamed <olamohamedola276@gmail.com>
2023-08-11 12:03:01 -0700

    commit message

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,17 @@
 int main(void)
 {
 int i, j;
-for (i = 0; i < 10; i++)
+for (i = 0; i < 9; i++)
 {
 for (j = i + 1; j < 10; j++)
 {
-putchar (i + '0');
-putchar (j + '0');
+putchar(i + '0');
+putchar(j + '0');
 if (i != 8 || j != 9)
+{
 putchar(',');
 putchar(' ');
+}
 }
 }
 putchar('\n');
