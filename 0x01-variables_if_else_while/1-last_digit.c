29389a938a2a2c65650df9d587ce7727e4afb211
ola mohamed <olamohamedola276@gmail.com>
2023-08-11 05:46:07 -0700

    commit message

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main -Start the program
+* Description: Output a sintence if the last digit of n is +ve ,-ve,zero
+* Return: 0 if right
+*/
+int main(void)
+{
+int n;
+int last;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+last = n % 10;
+if (last > 5)
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last);
+else if (last == 0)
+printf("Last digit of %i is 0 and is 0\n", n);
+else
+printf("last digit of %i is %i and is less than 6 and not 0\n", n, last);
+return (0);
+}
