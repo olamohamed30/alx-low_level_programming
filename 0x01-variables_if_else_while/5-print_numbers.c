5e14a182486d85936d914012817986411ffd775a
ola mohamed <olamohamedola276@gmail.com>
2023-08-11 10:29:33 -0700

    commit message

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Start the program
+ * Description: print all nom
+ * Return: 0 if right
+ */
+int main(void)
+{
+for (int i = 0, i < 10, i++)
+printf("%d", i);
+printf("\n");
+return (0);
+}
