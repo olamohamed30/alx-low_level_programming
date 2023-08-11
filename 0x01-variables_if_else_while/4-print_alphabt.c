97b1d99d3316ede24d8104374e09b3fd0ce963bd
ola mohamed <olamohamedola276@gmail.com>
2023-08-11 10:15:41 -0700

    commit message

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Start the program
+ * Description: print all alphapitic letters
+ * Return: 0 if right
+ */
+int main(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+if (letter == 'e' || letter == 'q')
+break;
+putchar(letter); /* Print letter */
+letter++; /* next letter */
+}
+putchar('\n'); /* newline after the loop */
+return (0);
+}
