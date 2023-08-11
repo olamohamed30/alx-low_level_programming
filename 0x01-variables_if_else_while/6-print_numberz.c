04fdb11f9b6707d412a4cdce50603618d7ff8faf
ola mohamed <olamohamedola276@gmail.com>
2023-08-11 10:42:50 -0700

    commit message

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Start the program
+ * Description: print all nom
+ * Return: 0 if right
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+putchar(i + '0');
+putchar('\n');
+return (0);
+}
