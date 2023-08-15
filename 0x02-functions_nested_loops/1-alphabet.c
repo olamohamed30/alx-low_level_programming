2236a5116aa84e05f9d25c43dcae80f0160d9715
ola mohamed <olamohamedola276@gmail.com>
2023-08-14 22:47:53 -0700

    commit message

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet+ newline
+ */
+void print_alphabet(void)
+{
+int char;
+for (char = 'a'; char <= 'z'; char++)
+_putchar(char);
+_putchar('\n');
+}
