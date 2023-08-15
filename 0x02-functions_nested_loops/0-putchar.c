9d69458a5d3ca661e87db7f08f10d30264a9e487
ola mohamed <olamohamedola276@gmail.com>
2023-08-14 22:34:16 -0700

    commit message

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: Prints "_putchar" + a newline
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char str[] = "_putchar\n";
+int c;
+for (i = 0; str[c] != '\0'; i++)
+{
+_putchar(str[c]);
+}
+return (0);
+}
