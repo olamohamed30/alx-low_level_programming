f8e1f41241b5ae9396e9e305a64632b75d3fb407
ola mohamed <olamohamedola276@gmail.com>
2023-08-15 05:36:08 -0700

    commit message

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the 10 times alphabet+ newline
+ */
+void print_alphabet(void)
+{
+int i;
+char cha;
+for (i = 0; i < 10 ; i++)
+{
+for (cha = 'a'; cha <= 'z'; cha++)
+_putchar(char);
+}
+_putchar('\n');
+}
