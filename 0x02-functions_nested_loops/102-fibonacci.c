8f161783af5c99aac86db737db8b7b7db601a27e
ola mohamed <olamohamedola276@gmail.com>
2023-08-15 16:58:46 -0700

    commit message

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *main- entry to program
+ *Return: 0 if right, any value if not.
+ */
+int main(void)
+{
+int  n1 = 1, n2 = 2;
+printf("%d, %d", n1, n2);
+for (int i = 3; i <= 50; i++)
+{
+int ne = n1 + n2;
+printf(", %d", ne);
+n1 = n2;
+n2 = ne;
+}
+printf("\n");
+return (0);
+}
