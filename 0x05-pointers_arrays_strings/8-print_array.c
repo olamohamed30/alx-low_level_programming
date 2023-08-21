1e4d948f00cb9acebc88fddece888bbd96fc6500
ola mohamed <olamohamedola276@gmail.com>
2023-08-21 14:12:25 -0700

    commit message

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+include "main.h"
+/**
+ * print_array - Prints n elements of an array.
+ * @a: Point array.
+ * @n: Num of elements
+ */
+void print_array(int *a, int n)
+{
+int in;
+
+for (in = 0; in < n; in++)
+{
+printf("%d", a[in]);
+if (in != n - 1)
+printf(", ");
+}
+printf("\n");
+}
