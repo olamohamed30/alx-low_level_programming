cbecc9ab339248260c60770ad457dbebb667d9ff
ola mohamed <olamohamedola276@gmail.com>
2023-08-21 17:01:53 -0700

    commit message

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,15 +4,19 @@
 
 int main(void)
 {
-	char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-	srand(time(NULL));
-	int l = 10;
-	int i;
-	for (i = 0; i < l; i++)
-	{
-		int randomIndex = rand() % (sizeof(charset) - 1);
-		printf("%c", charset[randomIndex]);
-		printf("\n");
-		return 0;
-	}
+	    char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	        int l = 10;
+		    int randomIndex;
+		        int i;
+
+			    srand(time(NULL));
+
+			        for (i = 0; i < le; i++) 
+					    {
+						            randomIndex = rand() % (sizeof(charset) - 1);
+							            printf("%c", charset[randomIndex]);
+								        }
+				    printf("\n");
+				        return 0;
 }
+
