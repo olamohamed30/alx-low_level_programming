ba1f2c8e8f0b5a106105e02098df51e630651800
ola mohamed <olamohamedola276@gmail.com>
2023-08-28 14:10:48 -0700

    commit message

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *  * _strchr-  memory with  byte.
+ *   * @s: Con byte.
+ *    * @c: Num of bytes .
+ *     * Return: dest.
+ */
+char *_strchr(char *s, char c)
+{
+	int in;
+
+	for (in = 0; s[in] != '\0'; in++)
+	{
+		if (s[in] == c)
+		{
+			return (&s[in]);
+		}
+	}
+	return (NULL);
+}
