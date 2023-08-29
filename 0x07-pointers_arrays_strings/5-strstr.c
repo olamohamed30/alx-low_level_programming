d3b95ba9938f146fdeecfe7c38e939a198ecd114
ola mohamed <olamohamedola276@gmail.com>
2023-08-28 17:56:22 -0700

    commit message

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,12 +1,11 @@
 #include "main.h"
 #include <stddef.h>
-
 /**
- *_strstr-  memory with  byte.
- *@haystack: Con byte.
- *@needle: Num of bytes .
- *Return: dest.
-   */
+ * _strstr - Locates a sub
+ *@haystack: The string
+ *@needle: The sub to search within haystack
+ *Return: Pointer to the beg of the located sub
+ */
 
 char *_strstr(char *haystack, char *needle)
 {
