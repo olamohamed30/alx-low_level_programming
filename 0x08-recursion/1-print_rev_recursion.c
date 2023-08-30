ce24f469b4977fc1c112267eede3b92f8a96e83b
ola mohamed <olamohamedola276@gmail.com>
2023-08-30 13:36:42 -0700

    commit message

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  * _print_rev_recursion - print a string backwords
+ *   * @st: string  printed
+ *    * Return: void/nothing
+    */
+void _print_rev_recursion(char *st)
+{
+	if (*st == '\0')
+	{
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
