7d3d39de11795e1d18a83b32c4e84604bb26988f
ola mohamed <olamohamedola276@gmail.com>
2023-09-01 13:00:54 -0700

    commit message

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point of fun
+ *   * @argc: The num of command line arg
+ *    * @argv: The command line arg
+ *     * Return: 0if Success
+ *      */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
