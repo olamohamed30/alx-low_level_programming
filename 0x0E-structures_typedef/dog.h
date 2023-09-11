eca0acd41dd3f82cdb93457ab56ab02cb4ae68d0
ola mohamed <olamohamedola276@gmail.com>
2023-09-11 02:28:37 -0700

    commit message

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *  * struct dog - Structure for a dog info
+ *   * @name: Name
+ *    * @age: Age dog
+ *     * @owner: Owner
+ *      * Description: structure to store
+   */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
