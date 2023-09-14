135c58086030c61633d8611aee5cfa143d098882
ola mohamed <olamohamedola276@gmail.com>
2023-09-14 11:14:24 -0700

    ola

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,49 +3,49 @@
 #include <stdio.h>
 
 /**
- * print_all - prints all
- * @format: list of typ of arg passedd
- */
+ *  * print_all - prints all.
+ *   * @format: list of types of arg
+     */
 void print_all(const char * const format, ...)
 {
-    va_list arg;
-    char *curr;
-    char *p = (char *)format;
-    int print = 0;
-    
-    va_start(arg, format);
-    while (*p)
-    {
-        if (print)
-            printf(", ");
-        switch (*p)
-        {
-            case 'c':
-                printf("%c", va_arg(arg, int));
-                print = 1;
-                break;
-            case 'i':
-                printf("%d", va_arg(arg, int));
-                print = 1;
-                break;
-            case 'f':
-                printf("%f", va_arg(arg, double));
-                print = 1;
-                break;
-            case 's':
-                curr = va_arg(arg, char*);
-                if (!curr)
-                    printf("(nil)");
-                else
-                    printf("%s", curr);
-                print = 1;
-                break;
-            default:
-                print = 0; /* Reset if nothing was printed */
-                break;
-        }
-        p++;
-    }
-    printf("\n");
-    va_end(arg);
+	va_list arg;
+	char *curr;
+	char *p = (char *)format;
+	int print = 0;
+
+	va_start(arg, format);
+	while (*p)
+	{
+		if (print)
+			printf(", ");
+		switch (*p)
+		{
+			case 'c':
+				printf("%c", va_arg(arg, int));
+				print = 1;
+				break;
+			case 'i':
+				printf("%d", va_arg(arg, int));
+				print = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(arg, double));
+				print = 1;
+				break;
+			case 's':
+				curr = va_arg(arg, char*);
+				if (!curr)
+					printf("(nil)");
+				else
+					printf("%s", curr);
+				print = 1;
+				break;
+			default:
+				print = 0;
+				break;
+		}
+		p++;
+	}
+	printf("\n");
+	va_end(arg);
 }
