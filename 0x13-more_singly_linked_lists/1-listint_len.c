519e2bfbfbfdf86890b69f16da5a6d339e2bf03d
ola mohamed <olamohamedola276@gmail.com>
2023-09-25 05:14:49 -0700

    ola any

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * listint_len - it is a singly linked list
+ * @h: int 
+ * Return: count of  node
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t c = 0;
+
+	while (h != NULL)
+	{
+		c++;
+		h = h->next;
+	}
+
+	return (c);
+}
