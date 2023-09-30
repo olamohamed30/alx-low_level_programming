b851cccfbd01b582e4cb7de980bfe9fa50cd3de1
ola mohamed <olamohamedola276@gmail.com>
2023-09-30 15:04:24 -0700

    ola

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+i#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * clear_bit - sets the vahjhkj,nkjnjk
+ * @n: pointjnmhjmhe number
+ * @indx: index,hjkjjk,.
+ * Return: 1 ijijk,mbtcfv
+ */
+int clear_bit(unsigned long int *n, unsigned int indx)
+{
+	unsigned long int m;
+
+	if (indx >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	m = 1UL << indx;
+	*n = *n & m;
+	return (1);
+}
