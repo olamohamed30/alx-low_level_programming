6f9053e3295f7092cac7d454ff429b0c63ff9c14
ola mohamed <olamohamedola276@gmail.com>
2023-09-30 14:45:47 -0700

    ahhh

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * set_bit - sets the vahjhkj,nkjnjk
+ * @n: pointjnmhjmhe number
+ * @indx: index,hjkjjk,.
+ * Return: 1 ijijk,mbtcfv
+ */
+int set_bit(unsigned long int *n, unsigned int indx)
+{
+	unsigned long int m;
+
+	if (indx >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	m = 1UL << indx;
+	*n |= m;
+
+	return (1);
+}
