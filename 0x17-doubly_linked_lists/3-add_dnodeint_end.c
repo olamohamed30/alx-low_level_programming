93e5fdd0f09c4e4cc7baa5fcdad1f05747a09ccb
ola mohamed <olamohamedola276@gmail.com>
2023-11-22 17:58:21 -0800

    ola

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,19 +1,28 @@
 #include <stdlib.h>
 #include "lists.h"
 
-dlistint_t *add_dnodeint(dlistint_t **head, const int n) {
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n) {
     dlistint_t *new_node = malloc(sizeof(dlistint_t));
+    dlistint_t *temp = *head;
+
     if (new_node == NULL) {
         return NULL;
     }
 
     new_node->n = n;
-    new_node->prev = NULL;
-    new_node->next = *head;
-    if (*head != NULL) {
-        (*head)->prev = new_node;
+    new_node->next = NULL;
+    if (*head == NULL) {
+        new_node->prev = NULL;
+        *head = new_node;
+        return new_node;
     }
-    *head = new_node;
+
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    temp->next = new_node;
+    new_node->prev = temp;
 
     return new_node;
 }
--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++
@@ -1,9 +0,0 @@
-void free_dlistint(dlistint_t *head) {
-    dlistint_t *temp;
-
-    while (head != NULL) {
-        temp = head;
-        head = head->next;
-        free(temp);
-    }
-}
